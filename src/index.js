31eb9a10c688cd775d8e4048de46c59a0be582d8
lorexp <vmartins@edu.univali.br>
2021-04-17 15:21:22 -0300

    add import of router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
-
+import router from './routes/index.js';
 const app = express();
 
 const day = 24 * 60 * 60 * 1000;
@@ -13,14 +13,6 @@ app.use(
   })
 );
 
-app.get('/:name', async (req, res) => {
-  const { name } = req.params;
-  req.session.name = name;
-  return res.send('Hello World');
-});
-
-app.get('/check/name', async (req, res) => {
-  return res.send(req.session.name);
-});
+app.use('/api', router);
 
 app.listen(3000, () => console.log('Running at port 3000'));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
-import express from 'express';
+import { Router } from 'express';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/:name', async (req, res) => {
   const { name } = req.params;
