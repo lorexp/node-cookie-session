717302570faf97bbc60acd8864922e0d288fa8f3
lorexp <vmartins@edu.univali.br>
2021-04-17 19:19:35 -0300

    add helmet'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import cookieSession from 'cookie-session';
 import dotenv from 'dotenv';
+import helmet from 'helmet';
 import router from './routes/index';
 
 dotenv.config();
 
 const { KEYS, PORT } = process.env;
 const app = express();
+
+app.use(helmet());
 
 const day = 24 * 60 * 60 * 1000;
 
