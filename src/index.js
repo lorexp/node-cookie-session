e9e141e18830324c5755e942b6e863a5147db2fd
lorexp <vmartins@edu.univali.br>
2021-04-17 15:12:06 -0300

    create two routes to set user name in session

--- src/index.js
+++ src/index.js
@@ -13,4 +13,14 @@ app.use(
   })
 );
 
+app.get('/:name', async (req, res) => {
+  const { name } = req.params;
+  req.session.name = name;
+  return res.send('Hello World');
+});
+
+app.get('/check/name', async (req, res) => {
+  return res.send(req.session.name);
+});
+
 app.listen(3000, () => console.log('Running at port 3000'));
